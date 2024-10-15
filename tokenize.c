e59ed03ce9a214187d8ba141f71508659c206b7f
krishguptacodes <139714105+krishguptacodes@users.noreply.github.com>
2024-10-15 16:19:28 -0400

    1.2 code added

--- tokenize.c
+++ tokenize.c
@@ -10,7 +10,6 @@ char** allocate_tokens(int size) {
     return (char **)malloc(size * sizeof(char *));
 }
 
-
 char** resize_tokens(char** tokens, int *size) {
     *size *= 2;  // Double the current size
     return (char **)realloc(tokens, (*size) * sizeof(char *));
@@ -100,22 +99,58 @@ char** tokenize(char* input) {
     return tokens;
 }
 
+// functionality of execute_command
+void execute_command(char** args) {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("Fork Failed");
+        exit(1);
+    } 
+    else if (pid==0) {
+        if (execvp(args[0], args) == -1) {
+            print(stderr, "%s: command not found\n", args[0]);
+            exit(1);
+ }    } else {
+        wait(NULL);
+    }
+}
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <input_string>\n", argv[0]);
-        return 1;
+    char input[INITIAL_INPUT_SIZE];
+
+    // welcome message
+    printf("Welcome to the mini-shell.");
+
+    while (1) {
+        printf("shell $");
+        fflush(stdout);
+
+        // handles Ctrl-D and if input is Exit
+        if ((fgets(input, INITIAL_INPUT_SIZE, stdin) == NULL) || (strcmp(input, "exit") == 0)) {
+            print ("Bye bye \n");
+            break;
+        }
+
+        //remove newline character from input
+        input[strcspn(input, "\n")] = 0;
+
+        // tokenize the input into command and arguments
+        char** args = tokenize(input);
+
+        if (args[0] == NULL) {
+            free(args);
+            continue;
+        }
+
+        execute_command(args);
+    
+        // free memory space after printing out the tokens
+        for (int i = 0; args[i] != NULL; i++) {
+            free(args[i]);
+        }
+        free(args);
     }
-
-    char *input = argv[1];
-
-    char **tokens = tokenize(input);
-
-    // free memory space after printing out the tokens
-    for (int i = 0; tokens[i] != NULL; i++) {
-        printf("%s\n", tokens[i]);
-        free(tokens[i]);
-    }
-
-    free(tokens);
     return 0;
 }
+
